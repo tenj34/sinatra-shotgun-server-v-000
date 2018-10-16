d80062cc4a124ca162c90923bd7b6222821e21a2
Juan Tenezaca <tenj34@gmail.com>
2018-10-16 21:47:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! Let's get this money"
+    "Welcome to your app!!!! Let's get this bread"
   end
 
 end
